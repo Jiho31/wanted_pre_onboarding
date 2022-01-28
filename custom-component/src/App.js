4b9b05aca12d347b5c18798ac2497671c3cf1759
Jiho Bok <bokjiho@gmail.com>
2022-01-28 15:11:47 +0900

    Chore: 리액트 설치 및 초기 환경 설정

---
+++ custom-component/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Tab from "./component/Tab";
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <Tab />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ custom-component/src/component/Tab.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Tab = () => {
+  return <div>tab indexes will be here</div>;
+};
+
+export default Tab;
