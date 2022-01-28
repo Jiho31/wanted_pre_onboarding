37eb17e112f5e00187c6ecc0bd3f3d64eef49000
Jiho Bok <bokjiho@gmail.com>
2022-01-28 23:57:31 +0900

    Feat: Tab 컴포넌트 구현

--- custom-component/src/App.js
+++ custom-component/src/App.js
@@ -1,12 +1,39 @@
 import "./App.css";
+import React, { useState } from "react";
 import Tab from "./component/Tab";
 
+const tabElements = [
+  { id: 0, content: "tab1" },
+  { id: 1, content: "tab2" },
+  { id: 2, content: "tab3" },
+];
+
 function App() {
+  const [tabIndex, setTabIndex] = useState(0);
+
+  const renderElement = (idx) => {
+    setTabIndex(idx);
+  };
+
   return (
     <div className="App">
       <header>
-        <Tab />
+        <Tab tabEventHandler={renderElement} currentIndex={tabIndex} />
       </header>
+      <main>
+        <div>
+          {tabElements[tabIndex].content}
+          {/* 탭 내용 템플릿 */}
+          {/* <article>
+            <h1>title 1</h1>
+            <section>content1</section>
+          </article>
+          <article>
+            <h1>title 2</h1>
+            <section>content2</section>
+          </article> */}
+        </div>
+      </main>
     </div>
   );
 }
--- custom-component/src/component/Tab.js
+++ custom-component/src/component/Tab.js
@@ -1,7 +1,62 @@
 import React from "react";
+import styled from "styled-components";
 
-const Tab = () => {
-  return <div>tab indexes will be here</div>;
+const Tab = ({ tabEventHandler, currentIndex }) => {
+  const onClickHandler = (idx) => {
+    tabEventHandler(idx);
+  };
+
+  return (
+    <NavTab>
+      <ul>
+        <TabElement
+          className={currentIndex === 0 ? "isActive" : ""}
+          onClick={() => onClickHandler(0)}
+        >
+          Tab1
+        </TabElement>
+        <TabElement
+          className={currentIndex === 1 ? "isActive" : ""}
+          onClick={() => onClickHandler(1)}
+        >
+          Tab2
+        </TabElement>
+        <TabElement
+          className={currentIndex === 2 ? "isActive" : ""}
+          onClick={() => onClickHandler(2)}
+        >
+          Tab3
+        </TabElement>
+      </ul>
+    </NavTab>
+  );
 };
+
+const NavTab = styled.nav`
+  width: 900px;
+  display: flex;
+  justify-content: center;
+  margin: 0 auto;
+  border-bottom: 1px solid #e0e0e0;
+
+  ul {
+    display: flex;
+    flex-direction: row;
+  }
+`;
+const TabElement = styled.li`
+  padding: 10px 20px;
+  border: 2px solid #fff;
+  cursor: pointer;
+  font-size: 15px;
+
+  &:hover {
+    border-bottom: 2px solid #d9d9d9;
+  }
+
+  &.isActive {
+    border-bottom: 2px solid #ffd952;
+  }
+`;
 
 export default Tab;
