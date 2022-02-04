988c101dedfde2931ab731a19568cbc370608bf9
Jiho Bok <bokjiho@gmail.com>
2022-02-04 13:35:02 +0900

    Feat: ClickToEdit 기능 구현

--- custom-component/src/component/ClickToEdit.js
+++ custom-component/src/component/ClickToEdit.js
@@ -1,26 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
+// import useInputs from "../hooks/useInputs";
+
+const initialValue = {
+  name: "복지호",
+  age: "2n",
+  mbti: "ISFJ",
+};
 
 const ClickToEdit = () => {
+  // const [{ name, age, mbti }, onChange] = useInputs({
+  //   name: "복지호",
+  //   age: "2n",
+  //   mbti: "ISFJ",
+  // });
+  const [inputs, setInputs] = useState(initialValue);
+  const [form, setForm] = useState(initialValue);
+
+  const onChangeHandler = (e) => {
+    const { id, value } = e.target;
+    setInputs((prevInputs) => ({ ...prevInputs, [id]: value }));
+  };
+  const onBlurHandler = (e) => {
+    const { id, value } = e.target;
+    setForm((prevForm) => ({ ...prevForm, [id]: value }));
+  };
+
   return (
     <Container>
       <h2>ClickToEdit</h2>
       <form>
         <FormElement>
           <label htmlFor="name">이름</label>
-          <input id="name" type="text" />
+          <input
+            id="name"
+            type="text"
+            value={inputs.name}
+            onChange={onChangeHandler}
+            onBlur={onBlurHandler}
+          />
         </FormElement>
         <FormElement>
           <label htmlFor="age">나이</label>
-          <input id="age" type="text" />
+          <input
+            id="age"
+            type="text"
+            value={inputs.age}
+            onChange={onChangeHandler}
+            onBlur={onBlurHandler}
+          />
         </FormElement>
         <FormElement>
           <label htmlFor="mbti">MBTI</label>
-          <input id="mbti" type="text" />
+          <input
+            id="mbti"
+            type="text"
+            value={inputs.mbti}
+            onChange={onChangeHandler}
+            onBlur={onBlurHandler}
+          />
         </FormElement>
       </form>
       <Result>
-        <p>이름 어쩌구 랄랄라 내용</p>
+        <p>
+          안녕하세요! 저는 {form.name}입니다.
+          <br />
+          나이는 {form.age}살이고 MBTI는 {form.mbti}입니다.
+          <br />
+          오늘도 행복한 하루 되세요! 😁
+        </p>
       </Result>
     </Container>
   );
@@ -49,10 +97,16 @@ const FormElement = styled.div`
     width: 180px;
     height: 30px;
     padding: 5px 8px;
+    border: 1px solid rgb(118, 118, 118);
+    border-radius: 5px;
+  }
+  input:focus {
+    border: 2px solid #ffd952;
   }
 `;
 const Result = styled.div`
   display: flex;
+  margin: 0 auto;
 `;
 
 export default ClickToEdit;
